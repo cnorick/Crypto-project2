51ec6351e960f64fc29cffdc199d9721e6838943
Orick <corick@vols.utk.edu>
2017-10-30 20:51:18 -0400

    Add decryption method and test

--- rsa-enc.py
+++ rsa-enc.py
@@ -27,11 +27,26 @@ def addRandom(m, key):
         raise TypeError('m must be of type int')
 
     numRandBits = key.numBits // 2
-    numMessageBits = key.numBits - numRandBits
+    numMessageBits = key.numBits - numRandBits - 2
     r = getRandom(numRandBits)
 
     # Concat the randomness and the message.
     return (r << numMessageBits) | m
+
+'''
+Removes the randomness added to m by addRandom().
+'''
+def removeRandom(m, key):
+    if type(key) is not Key:
+        raise TypeError('key must be of type Key')
+    if type(m) is not int:
+        raise TypeError('m must be of type int')
+
+    numRandBits = key.numBits // 2
+    numMessageBits = key.numBits - numRandBits - 2
+
+    # Remove the randomness from the message.
+    return ((2 ** numMessageBits) - 1) & m
 
 '''
 Performs modular exponentiation.
@@ -42,8 +57,7 @@ def modExp(m, key):
         raise TypeError('key must be of type Key')
     if type(m) is not int:
         raise TypeError('m must be of type int')
-
-    return (m ** key.ed) % key.N
+    return pow(m, key.ed, key.N)
 
 '''
 encrypts <m> with public key <key> to element in ZN*.
@@ -59,21 +73,47 @@ def enc(m, key):
 
     return modExp(mhat, key)
 
+def dec(c, key):
+    if type(key) is not Key:
+        raise TypeError('key must be of type Key')
+    if type(c) is not int:
+        raise TypeError('m must be of type int')
+
+    mhat = modExp(c, key)
+
+    return removeRandom(mhat, key)
+
+def test():
+    N = 3233
+    numBits = 12
+    for message in range(2**(numBits - numBits//2 - 2)):
+        privKey = Key(12, N, 413)
+        pubKey = Key(12, N, 17)
+        e = enc(message, pubKey)
+        d = dec(e, privKey)
+        if d != message:
+            print('d: {d}, message: {message}'.format(d=d, message=message))
+            raise Exception("IT'S BROKEN")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("usage: python rsa-enc.py <keyFile> <inputFile> <outputFile>")
+    if len(sys.argv) != 5:
+        print("usage: python rsa-enc.py <e|d> <keyFile> <inputFile> <outputFile>")
         sys.exit()
 
-    (keyFileName, inputFileName, outputFileName) = sys.argv[1:4]
+    (mode, keyFileName, inputFileName, outputFileName) = sys.argv[1:5]
 
     with open(keyFileName, 'r') as keyFile:
-        (numBits, N, e) = [int(line) for line in keyFile.readlines()]
+        (numBits, N, ed) = [int(line) for line in keyFile.readlines()]
 
     with open(inputFileName, 'r') as inputFile:
         message = int(inputFile.read())
 
-    print(enc(message, Key(numBits, N, e)))
+    if mode == 'e':
+        output = enc(message, Key(numBits, N, ed))
+    elif mode == 'd':
+        output = dec(message, Key(numBits, N, ed))
+    else:
+        raise ValueError("mode must be e (encrypt) or d (decrypt)")
     
-    # with open(outputFileName, 'w') as outputFile:
-    #     outputFile.write()
+    with open(outputFileName, 'w') as outputFile:
+        outputFile.write(str(output))
